f820d82fb71caa6267c5026fa18746a99974691b
Titus Wormer <tituswormer@gmail.com>
2023-01-06 13:17:01 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,22 +1,21 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {u} from 'unist-builder'
 import {h} from 'hastscript'
 import {phrasing} from './index.js'
 
-test('phrasing()', (t) => {
-  t.notOk(phrasing(h('div', 'Alpha')), 'flow')
-  t.ok(phrasing(h('meta', {itemProp: 'bravo'})), 'meta w/ itemProp')
-  t.notOk(phrasing(h('meta', {charSet: 'utf8'})), 'meta w/o itemProp')
-  t.ok(phrasing(u('text', 'charlie')), 'text')
-  t.ok(phrasing(u('text', '\n\t')), 'inter-element white-space')
-  t.ok(phrasing(h('a', {href: '/'}, 'Delta')), 'listed elements (a)')
-  t.ok(phrasing(h('b', {id: 'echo'}, 'Foxtrott')), 'listed elements (b)')
-  t.ok(phrasing(h('wbr')), 'listed elements (wbr)')
-  t.ok(phrasing(h('svg')), 'embedded content')
-  t.ok(
+test('phrasing()', () => {
+  assert.ok(!phrasing(h('div', 'Alpha')), 'flow')
+  assert.ok(phrasing(h('meta', {itemProp: 'bravo'})), 'meta w/ itemProp')
+  assert.ok(!phrasing(h('meta', {charSet: 'utf8'})), 'meta w/o itemProp')
+  assert.ok(phrasing(u('text', 'charlie')), 'text')
+  assert.ok(phrasing(u('text', '\n\t')), 'inter-element white-space')
+  assert.ok(phrasing(h('a', {href: '/'}, 'Delta')), 'listed elements (a)')
+  assert.ok(phrasing(h('b', {id: 'echo'}, 'Foxtrott')), 'listed elements (b)')
+  assert.ok(phrasing(h('wbr')), 'listed elements (wbr)')
+  assert.ok(phrasing(h('svg')), 'embedded content')
+  assert.ok(
     phrasing(h('link', {rel: ['stylesheet'], href: 'index.css'})),
     'body-ok links'
   )
-
-  t.end()
 })
