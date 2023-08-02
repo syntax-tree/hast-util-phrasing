ed14f6a510f09307fd8de982020b56e87bce7942
Titus Wormer <tituswormer@gmail.com>
2023-08-02 14:59:47 +0200

    Change to use `exports`

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
 import {h} from 'hastscript'
+import {phrasing} from 'hast-util-phrasing'
 import {u} from 'unist-builder'
-import {phrasing} from './index.js'
 
 test('phrasing()', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+    assert.deepEqual(Object.keys(await import('hast-util-phrasing')).sort(), [
       'phrasing'
     ])
   })
